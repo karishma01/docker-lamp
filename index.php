3f05fe4d5c15c56e5abf8d602deab280aaf261ef
root <root@ip-172-31-23-43.us-west-1.compute.internal>
2018-06-25 08:33:05 +0000

    edited the index page

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 # Sample index.php file for fauria/lamp Docker image.
 # To use your own website instead, please refer to the documentation:
 # https://registry.hub.docker.com/u/fauria/lamp/
-Welcome to Lamp Demo.
+echo "Welcome to Lamp Demo.";
 
 ?>
