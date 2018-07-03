26434a139b633d62dd5566edf85dd16d9c42584a
karishma01 <33022533+karishma01@users.noreply.github.com>
2018-07-03 19:26:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 # Sample index.php file for fauria/lamp Docker image.
 # To use your own website instead, please refer to the documentation:
 # https://registry.hub.docker.com/u/fauria/lamp/
-echo "Hello Everyone!!!!!........Welcome to Lamp Demo..........!!";
+echo "Hello Everyone!!!!!........Welcome to Lamp Demo..........!!!!!!!!";
 
 ?>
